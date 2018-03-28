a3f2a43486312da44ec90e89c87e59163c39f236
Michal Harish <michal.harish@gmail.com>
2018-03-28 19:12:54 +0100

    more reliable way of doing restart-or-start

--- lib/cd/include.sh
+++ lib/cd/include.sh
@@ -4,6 +4,5 @@ APPLICABLE_SERVICES+=("cd")
 
 start_cd() {
     #cd service restart instead of stop-start because the running /opt/bash-cd-server.sh gets interrupted
-    restart -q cd
-    start -q cd
+    restart -q cd || start -q cd
 }
