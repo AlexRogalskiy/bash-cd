4bac0bae91b35ebf0473224a3a58a7d06c54243b
michal-harish <michal.harish@gmail.com>
2018-04-04 11:35:26 +0100

    git watch altenrative for the bash-cd-server

--- lib/cd/opt/bash-cd-server.sh
+++ lib/cd/opt/bash-cd-server.sh
@@ -13,16 +13,18 @@ exec 2>&1
 handle() {
     cd /opt/bash-cd
     read in
-    echo "$in"
     if [[ "$in" == "POST /push"* ]]; then
-        git remote update
+        branch=$(git rev-parse --abbrev-ref HEAD)
+        local_revision=$(git rev-parse $branch)
+        git remote update &> /dev/null
+        remote_revision=$(git rev-parse origin/$branch)
         rollback_file="$DIR/unclean"
         declare changed=0
         if [ -f "$rollback_file" ]; then
             warn "RESUMING INCOMPLETE BUILD: $(cat $rollback_file)"
             changed=1
-        elif [[ "$(git status | grep 'Your branch is behind')" == "Your branch is behind"* ]]; then
-            highlight "ENVIRONMENT CHANGE DETECTED (POST /PUSH)"
+        elif [ $local_revision != $remote_revision ]; then
+            highlight "ENVIRONMENT CHANGE DETECTED ($in), LOCAL: $local_revision, REMOTE: $remote_revision"
             changed=1
             echo $(git rev-parse HEAD) > $rollback_file
         fi
@@ -32,8 +34,6 @@ handle() {
             ./apply.sh setup
             ./apply.sh install
             rm $rollback_file
-        else
-            echo "No changes to apply."
         fi
     elif [[ "$in" == "POST /install"* ]]; then
         ./apply.sh install
@@ -46,3 +46,10 @@ highlight "BASH-CD SERVER LISTENING ON PORT: $CD_PORT"
 echo "POST /push" | handle # trigger initial build that may need resuming after reboot
 RESPONSE="HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n${2:-"OK"}\r\n"
 while { echo -en "$RESPONSE"; } | nc -l "${1:-$CD_PORT}" | handle; do sleep 3; done
+
+## use the following code instead of the 4-lines above if you don't have a way of receiving webhook HTTP POST requests
+#highlight "BASH-CD SERVER WATCHING REMOTE GIT REPO"
+#while [ 1 ]; do
+#    echo "POST /push BASH/LOOP" | handle
+#    sleep 15
+#done
