c76100f4045120ffeff219442943cf24b1e7a3ae
Michal Harish <michal.harish@gmail.com>
2018-08-16 18:25:53 +0100

    cleaning up avro formatter previous builds to prevent incompatible libs during kafka upgrade

--- lib/kafka-cli/include.sh
+++ lib/kafka-cli/include.sh
@@ -16,7 +16,8 @@ done
 function install_kafka-cli() {
     git_clone_or_update https://github.com/amient/affinity.git "$AFFINITY_HOME" "master"
     cd $AFFINITY_HOME
-    echo "installing avro formatter.."
+    echo "installing affinity avro formatter.."
+    ./gradlew --no-daemon :kafka:avro-formatter-kafka:clean
     ./gradlew --no-daemon :kafka:avro-formatter-kafka:build --exclude-task test
     rm  /opt/kafka/current/libs/avro-formatter-kafka-*
     cp ./kafka/avro-formatter-kafka/build/lib/avro-formatter-kafka-*-all.jar /opt/kafka/current/libs/
