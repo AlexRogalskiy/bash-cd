d459e9e5a1e3e8a17081b2b75340b2c0216066b7
Michal Harish <michal.harish@gmail.com>
2018-08-17 15:11:13 +0100

    kafka-distro installs directly into /opt/kafka

--- lib/kafka-distro/include.sh
+++ lib/kafka-distro/include.sh
@@ -7,11 +7,9 @@ KAFKA_PACKAGE="kafka_2.11-$KAFKA_VERSION"
 APPLICABLE_SERVICES+=("kafka-distro")
 
 build_kafka-distro() {
-    checkvar BUILD_DIR
-    kafka_home="$BUILD_DIR/opt/kafka/$KAFKA_PACKAGE"
-    download "http://www.mirrorservice.org/sites/ftp.apache.org/kafka/$KAFKA_VERSION/$KAFKA_PACKAGE.tgz" "$BUILD_DIR/opt/kafka"
-    if [ ! -d "$kafka_home" ]; then
-        tar -xzf "$BUILD_DIR/opt/kafka/$KAFKA_PACKAGE.tgz" -C "$BUILD_DIR/opt/kafka"
+    download "http://www.mirrorservice.org/sites/ftp.apache.org/kafka/$KAFKA_VERSION/$KAFKA_PACKAGE.tgz" "/opt/kafka"
+    if [ ! -d "/opt/kafka/$KAFKA_PACKAGE" ]; then
+        tar -xzf "/opt/kafka/$KAFKA_PACKAGE.tgz" -C "/opt/kafka"
     fi
 }
 
