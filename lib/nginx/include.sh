04b80eea97922b1712d6e3066138e5f0708cbcf0
Michal Harish <michal.harish@gmail.com>
2018-11-03 19:29:09 +0000

    nginx

---
+++ lib/nginx/include.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+checkvar WEB_SERVER
+
+if [ "$WEB_SERVER" == "$PRIMARY_IP" ]; then
+    APPLICABLE_SERVICES+=("nginx")
+fi
+
+install_nginx() {
+    apt-get -y -o Dpkg::Options::=--force-confdef install nginx
+    mkdir -p /data/log/nginx
+    chown -R www-data:www-data /data/log/nginx
+}
+
+start_nginx() {
+    systemctl start nginx
+}
+
+stop_nginx() {
+    systemctl stop nginx
+}
+
