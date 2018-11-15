05b7c73aeb2b544ad70dc046b726f7074423ea3e
michal-harish <michal.harish@gmail.com>
2018-11-15 17:48:57 +0000

    centos sshd

--- lib/cd/include.sh
+++ lib/cd/include.sh
@@ -14,5 +14,5 @@ install_cd() {
 #cd service restart instead of stop-start because the running /opt/bash-cd-server.sh gets interrupted
 start_cd() {
     systemctl restart cd.service || systemctl start cd.service
-    systemctl start ssh
+    systemctl start sshd
 }
